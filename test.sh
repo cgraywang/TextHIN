887e85129e1563c38b8eea12a24dc4262187eacb
Haoran Li <lhrshitc@163.com>
2016-07-24 17:02:18 +0800

    version1.0

--- test.sh
+++ test.sh
@@ -5,9 +5,8 @@ class="edu.pku.dlib.KnowSim.Main"
 args="-Grammar.inPaths lib/models/15.exec/grammar "\
 "-FeatureExtractor.featureDomains entityAlignmentFeature "\
 "-EntityLexicon.freebaseDomainPath test/validDomains.txt "\
-"-inFile test/testdocs.txt "\
+"-EntityLinkingResultPath test/testdoc.linking.txt "
+io_args="-inFile test/testdocs.txt "\
 "-outFile test/testdoc.sp.txt "\
 "-tokenOutFile test/test.token.txt "\
-"-EntityLinkingResultPath test/testdoc.linking.txt "
-java -cp libknowsim/knowsim.jar $jvm_args $class $args
-# java -cp libknowsim/knowsim.jar -ea -Dmodules=parser,core,freebase edu.pku.dlib.KnowSim.Main 
+java -cp libknowsim/knowsim.jar $jvm_args $class $args $io_args
